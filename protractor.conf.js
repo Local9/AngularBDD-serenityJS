398c8338a5957931a508f022593ebe9dc58edf95
Antony Cook <kinglo0py@gmail.com>
2018-01-05 14:03:54 +0000

    Updated packages

--- protractor.conf.js
+++ protractor.conf.js
@@ -13,21 +13,21 @@ exports.config = {
   // Framework definition - tells Protractor to use Serenity/JS
   framework: 'custom',
   frameworkPath: require.resolve('serenity-js'),
-  specs: [ 'features/**/*.feature' ],
+  specs: ['features/**/*.feature'],
 
   cucumberOpts: {
-    require:    [ 'features/**/step_definitions/*.ts' ], // loads step definitions
-    format:     'pretty',               // enable console output
-    compiler:   'ts:ts-node/register'   // interpret step definitions as TypeScript
+    require: ['features/**/step_definitions/*.ts'], // loads step definitions
+    format: 'pretty', // enable console output
+    compiler: 'ts:ts-node/register' // interpret step definitions as TypeScript
   },
 
   serenity: {
-    crew:    [
+    crew: [
       crew.serenityBDDReporter(),
       crew.photographer()
     ],
     dialect: 'cucumber',
-    stageCueTimeout: 30 * 1000   // up to 30 seconds by default
+    stageCueTimeout: 30 * 1000 // up to 30 seconds by default
   }
 
 };
