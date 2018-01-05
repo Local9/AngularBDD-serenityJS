5e07da8fe3aedfa941ab2156a06a62a7d657ba82
Antony Cook <kinglo0py@gmail.com>
2018-01-05 19:31:34 +0000

    selectScenario is working

--- features/welcome_page/step_definitions/welcome.steps.ts
+++ features/welcome_page/step_definitions/welcome.steps.ts
@@ -1,4 +1,4 @@
-import {browser, by, element} from "protractor";
+import {browser, by, element} from 'protractor';
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
 
@@ -8,13 +8,14 @@ const expect = chai.expect;
 export = function myStepDefinitions () {
 
   this.Given(/^I am on the homepage$/, function (callback) {
-    browser.get("/").then(callback());
+    browser.get('/').then(callback());
   });
 
 
   this.Then(/^I should see welcome message$/, function (callback) {
-    let el = element(by.css('app-root h1')).getText();
-    return expect(el).to.eventually.equal("Welcome to app!!").then(function () {
+    ngApimock.selectScenario('welcome', 'welcome');
+    const el = element(by.css('app-root h1')).getText();
+    return expect(el).to.eventually.equal('Welcome to app!!').then(function () {
       callback();
     }).catch(function (err) {
       callback(err);
