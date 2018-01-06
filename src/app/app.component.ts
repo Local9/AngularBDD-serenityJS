6eb5fcf103c24c024d8bfef76bae6f1633c2e138
Antony Cook <kinglo0py@gmail.com>
2018-01-06 17:05:12 +0000

    service added but API returning 404

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WelcomeService } from './service/welcome.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  constructor(welcomeService: WelcomeService) {
+    welcomeService.getWelcome()
+      .then(
+        data => this.title = data
+      );
+  }
 }
---
+++ src/app/service/welcome.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
+
+@Injectable()
+export class WelcomeService {
+  constructor(private http: Http) { }
+
+  getWelcome() {
+    return this.http.get('api/v1/welcome').map(response => response.json())
+    .catch((error: Response | any) => {
+      return Observable.throw(error.json());
+    }).toPromise();
+  }
+}
