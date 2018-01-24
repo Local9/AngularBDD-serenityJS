b6d3c1ee71155729161e2e881fff170ee42c7c1e
Antony Cook <kinglo0py@gmail.com>
2018-01-24 12:19:29 +0000

    Removed error where ngApimock cannot be found in karma tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,7 +25,6 @@ describe('AppComponent', () => {
 
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
-    ngApimock.selectScenario('welcome', 'welcome-new-title');
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!!');
